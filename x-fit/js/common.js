dc544a909d367b236e4577870396d15cb5884597
AvrilDgn <39597007+AvrilDgn@users.noreply.github.com>
2019-11-12 01:59:08 +0900

    x-fit upd4

--- x-fit/js/common.js
+++ x-fit/js/common.js
@@ -48,4 +48,15 @@ $('document').ready(function() {
 	}, function(){
 		hoverCell.removeClass("hoverCell");
 	});
+
+	//map
+	function initMap() {
+		var coordinates = {lat: 47.212325, lng: 38.933663},
+			map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 15,
+				center: coordinates,
+				scrollwheel: false,
+       			disableDefaultUI: true,
+			});
+	}
 });
