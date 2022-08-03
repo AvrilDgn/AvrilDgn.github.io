bc03f17f5c13a4fb28c4fa8f1aac87af9ac992e4
AvrilDgn <magnuspg07@gmail.com>
2022-08-04 01:13:05 +0900

    add new "zxc"

---
+++ zxc/assets/js/main.js
@@ -0,0 +1,357 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+class Modal {
+	constructor(props) {
+		const defaultConfig = {
+			backscroll: true,
+			linkAttrName: 'data-modal',
+			closeOnOverlay: true,
+			closeOnEsc: true,
+			closeBtnAttrName: 'data-modal-close',
+			catchFocus: true,
+			fixedSelectors: '*[data-fixed]',
+			beforeOpen: () => { },
+			afterClose: () => { },
+		};
+		this.config = Object.assign(defaultConfig, props);
+		if (this.config.linkAttrName) {
+			this.init();
+		}
+		this._closeAfterTransition = this._closeAfterTransition.bind(this);
+	}
+
+	init() {
+		this.isOpened = false;
+		this.openedWindow = false;
+		this.starter = false;
+		this._nextWindows = false;
+		this._scrollPosition = 0;
+		this._reopenTrigger = false;
+		this._overlayChecker = false;
+		this._isMoved = false;
+		this._form = null;
+		this._focusElements = [
+			'a[href]',
+			'area[href]',
+			'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+			'select:not([disabled]):not([aria-hidden])',
+			'textarea:not([disabled]):not([aria-hidden])',
+			'button:not([disabled]):not([aria-hidden])',
+			'iframe',
+			'object',
+			'embed',
+			'[contenteditable]',
+			'[tabindex]:not([tabindex^="-"])',
+		];
+
+		this.eventsFeeler();
+	}
+
+	eventsFeeler() {
+		document.addEventListener('click', (e) => {
+			const clickedlink = e.target.closest(`[${this.config.linkAttrName}]`);
+			if (!this._isMoved && clickedlink) {
+				e.preventDefault();
+				this.starter = clickedlink;
+				const targetSelector = this.starter.getAttribute(this.config.linkAttrName);
+				this._nextWindows = document.querySelector("#" + targetSelector);
+				this.open();
+				return;
+			}
+			if (e.target.closest(`[${this.config.closeBtnAttrName}]`)) {
+				this.close();
+			}
+		});
+
+		if (this.config.closeOnOverlay) {
+			document.addEventListener('mousedown', (e) => {
+				if (!this._isMoved && (e.target instanceof Element) && !e.target.classList.contains('modal__wrapper')) return;
+				this._overlayChecker = true;
+			});
+
+			document.addEventListener('mouseup', (e) => {
+				if (!this._isMoved && (e.target instanceof Element) && this._overlayChecker && e.target.classList.contains('modal__wrapper')) {
+					e.preventDefault();
+					this._overlayChecker = !this._overlayChecker;
+					this.close();
+					return;
+				}
+				this._overlayChecker = false;
+			});
+		}
+
+		window.addEventListener('keydown', (e) => {
+			if (!this._isMoved && this.config.closeOnEsc && e.which === 27 && this.isOpened) {
+				e.preventDefault();
+				this.close();
+				return;
+			}
+			if (!this._isMoved && this.config.catchFocus && e.which === 9 && this.isOpened) {
+				this.focusCatcher(e);
+			}
+		});
+	}
+
+	open(selector) {
+		if (selector) {
+			if (typeof (selector) === 'string') {
+				this._nextWindows = document.querySelector(selector);
+			} else {
+				this._nextWindows = selector;
+			}
+		}
+		if (!this._nextWindows) {
+			console.log('Warning: Modal selector is not found');
+			return;
+		}
+		if (this.isOpened) {
+			this._reopenTrigger = true;
+			this.close();
+			return;
+		}
+		this.openedWindow = this._nextWindows;
+
+		this._form = this.openedWindow.querySelector('form');
+
+		this.config.beforeOpen(this);
+		this._bodyScrollControl();
+
+		this.openedWindow.classList.add('modal--active');
+		this.openedWindow.setAttribute('aria-hidden', 'false');
+		if (this.config.catchFocus) this.focusControl();
+		this.isOpened = true;
+	}
+
+	close() {
+		if (!this.isOpened) {
+			return;
+		}
+
+		let hiddenInputs = this._form.querySelectorAll('input[data-form-data]');
+		hiddenInputs.forEach(input => {
+			this._form.removeChild(input);
+		});
+
+		this.openedWindow.classList.add('modal--moved');
+		this._isMoved = true;
+		this.openedWindow.addEventListener('transitionend', this._closeAfterTransition);
+		this.openedWindow.classList.remove('modal--active');
+	}
+
+	_closeAfterTransition() {
+		this.openedWindow.classList.remove('modal--moved');
+		this.openedWindow.removeEventListener('transitionend', this._closeAfterTransition);
+		this._isMoved = false;
+		this.openedWindow.setAttribute('aria-hidden', 'true');
+
+		if (this.config.catchFocus) this.focusControl();
+		this._bodyScrollControl();
+		this.isOpened = false;
+		this.openedWindow.scrollTop = 0;
+		this.config.afterClose(this);
+
+		if (this._reopenTrigger) {
+			this._reopenTrigger = false;
+			this.open();
+		}
+	}
+
+	focusControl() {
+		const nodes = this.openedWindow.querySelectorAll(this._focusElements);
+		if (this.isOpened && this.starter) {
+			this.starter.focus();
+		} else if (nodes.length) nodes[0].focus();
+	}
+
+	focusCatcher(e) {
+		const nodes = this.openedWindow.querySelectorAll(this._focusElements);
+		const nodesArray = Array.prototype.slice.call(nodes);
+		if (!this.openedWindow.contains(document.activeElement)) {
+			nodesArray[0].focus();
+			e.preventDefault();
+		} else {
+			const focusedItemIndex = nodesArray.indexOf(document.activeElement);
+			if (e.shiftKey && focusedItemIndex === 0) {
+				nodesArray[nodesArray.length - 1].focus();
+				e.preventDefault();
+			}
+			if (!e.shiftKey && focusedItemIndex === nodesArray.length - 1) {
+				nodesArray[0].focus();
+				e.preventDefault();
+			}
+		}
+	}
+
+	_bodyScrollControl() {
+		if (!this.config.backscroll) return;
+
+		const fixedSelectorsElems = document.querySelectorAll(this.config.fixedSelectors);
+		const fixedSelectors = Array.prototype.slice.call(fixedSelectorsElems);
+
+		const html = document.documentElement;
+		if (this.isOpened === true) {
+			html.classList.remove('modal-opened');
+			html.style.marginRight = '';
+			fixedSelectors.forEach((el) => {
+				el.style.paddingRight = '';
+			});
+			window.scrollTo(0, this._scrollPosition);
+			html.style.top = '';
+			return;
+		}
+		this._scrollPosition = window.pageYOffset;
+		const marginSize = window.innerWidth - html.clientWidth;
+		html.style.top = `${-this._scrollPosition}px`;
+
+		if (marginSize) {
+			html.style.marginRight = `${marginSize}px`;
+			fixedSelectors.forEach((el) => {
+				el.style.paddingRight = `${parseInt(getComputedStyle(el).paddingRight, 10) + marginSize}px`;
+			});
+		}
+		html.classList.add('modal-opened');
+	}
+}
+
+$(document).ready(function () {
+	/**
+	 * Input mask
+	 */
+
+	// $("input[name='phone']").mask("+7 (999) 999-99-99", { placeholder: "_" });
+
+
+	/**
+	 * Menu fixation
+	 */
+
+	let headerFixation = function () {
+		let header = $(".header");
+
+		if ($(window).scrollTop() > 100) {
+			header.addClass("header--fixed");
+		} else {
+			if (!$('html').hasClass('modal-opened')) {
+				header.removeClass("header--fixed");
+			}
+		}
+	}
+
+	headerFixation();
+	$(window).on("scroll", headerFixation);
+
+
+	/**
+	 * Anchor scroll
+	 */
+
+	// $(document).on("click", 'a[href^="#"]', function (e) {
+	// 	if ($(this).attr('href').length <= 1) return;
+
+	// 	e.preventDefault();
+
+	// 	$("html, body").animate({
+	// 		scrollTop: $($.attr(this, "href")).offset().top
+	// 	}, 500);
+	// });
+
+	
+	/**
+	 * Modal
+	 */
+
+	// let modals = new Modal();
+
+
+});
+
+/***/ })
+/******/ ]);
