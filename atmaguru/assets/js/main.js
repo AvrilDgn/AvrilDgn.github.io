bb6e9230d8d006dc9a761a78c421791ed9a4009d
AvrilDgn <magnuspg07@gmail.com>
2022-10-11 17:48:56 +0900

    add b24 form

--- atmaguru/assets/js/main.js
+++ atmaguru/assets/js/main.js
@@ -1,250 +1,250 @@
-class Modal {
-	constructor(props) {
-		const defaultConfig = {
-			backscroll: true,
-			linkAttrName: 'data-modal',
-			closeOnOverlay: true,
-			closeOnEsc: true,
-			closeBtnAttrName: 'data-modal-close',
-			catchFocus: true,
-			fixedSelectors: '*[data-fixed]',
-			beforeOpen: () => { },
-			afterClose: () => { },
-		};
-		this.config = Object.assign(defaultConfig, props);
-		if (this.config.linkAttrName) {
-			this.init();
-		}
-		this._closeAfterTransition = this._closeAfterTransition.bind(this);
-	}
+// class Modal {
+// 	constructor(props) {
+// 		const defaultConfig = {
+// 			backscroll: true,
+// 			linkAttrName: 'data-modal',
+// 			closeOnOverlay: true,
+// 			closeOnEsc: true,
+// 			closeBtnAttrName: 'data-modal-close',
+// 			catchFocus: true,
+// 			fixedSelectors: '*[data-fixed]',
+// 			beforeOpen: () => { },
+// 			afterClose: () => { },
+// 		};
+// 		this.config = Object.assign(defaultConfig, props);
+// 		if (this.config.linkAttrName) {
+// 			this.init();
+// 		}
+// 		this._closeAfterTransition = this._closeAfterTransition.bind(this);
+// 	}
 
-	init() {
-		this.isOpened = false;
-		this.openedWindow = false;
-		this.starter = false;
-		this._nextWindows = false;
-		this._scrollPosition = 0;
-		this._reopenTrigger = false;
-		this._overlayChecker = false;
-		this._isMoved = false;
-		this._form = null;
-		this._focusElements = [
-			'a[href]',
-			'area[href]',
-			'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
-			'select:not([disabled]):not([aria-hidden])',
-			'textarea:not([disabled]):not([aria-hidden])',
-			'button:not([disabled]):not([aria-hidden])',
-			'iframe',
-			'object',
-			'embed',
-			'[contenteditable]',
-			'[tabindex]:not([tabindex^="-"])',
-		];
+// 	init() {
+// 		this.isOpened = false;
+// 		this.openedWindow = false;
+// 		this.starter = false;
+// 		this._nextWindows = false;
+// 		this._scrollPosition = 0;
+// 		this._reopenTrigger = false;
+// 		this._overlayChecker = false;
+// 		this._isMoved = false;
+// 		this._form = null;
+// 		this._focusElements = [
+// 			'a[href]',
+// 			'area[href]',
+// 			'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+// 			'select:not([disabled]):not([aria-hidden])',
+// 			'textarea:not([disabled]):not([aria-hidden])',
+// 			'button:not([disabled]):not([aria-hidden])',
+// 			'iframe',
+// 			'object',
+// 			'embed',
+// 			'[contenteditable]',
+// 			'[tabindex]:not([tabindex^="-"])',
+// 		];
 
-		this.eventsFeeler();
-	}
+// 		this.eventsFeeler();
+// 	}
 
-	eventsFeeler() {
-		document.addEventListener('click', (e) => {
-			const clickedlink = e.target.closest(`[${this.config.linkAttrName}]`);
-			if (!this._isMoved && clickedlink) {
-				e.preventDefault();
-				this.starter = clickedlink;
-				const targetSelector = this.starter.getAttribute(this.config.linkAttrName);
-				this._nextWindows = document.querySelector("#" + targetSelector);
+// 	eventsFeeler() {
+// 		document.addEventListener('click', (e) => {
+// 			const clickedlink = e.target.closest(`[${this.config.linkAttrName}]`);
+// 			if (!this._isMoved && clickedlink) {
+// 				e.preventDefault();
+// 				this.starter = clickedlink;
+// 				const targetSelector = this.starter.getAttribute(this.config.linkAttrName);
+// 				this._nextWindows = document.querySelector("#" + targetSelector);
 
-				//feedback
-				let locationInput = this._nextWindows.querySelector('input[name="location"]');
-				if (locationInput) {
-					let location = this.starter.getAttribute("data-location");
-					locationInput.value = location;
-				}
+// 				//feedback
+// 				let locationInput = this._nextWindows.querySelector('input[name="location"]');
+// 				if (locationInput) {
+// 					let location = this.starter.getAttribute("data-location");
+// 					locationInput.value = location;
+// 				}
 
-				//employee
-				let employeeName = this._nextWindows.querySelector('.modal__name');
-				let employeePosition = this._nextWindows.querySelector('.modal__position');
-				let employeeDescription = this._nextWindows.querySelector('.modal__description');
+// 				//employee
+// 				let employeeName = this._nextWindows.querySelector('.modal__name');
+// 				let employeePosition = this._nextWindows.querySelector('.modal__position');
+// 				let employeeDescription = this._nextWindows.querySelector('.modal__description');
 
-				if (employeeName && employeePosition && employeeDescription) {
-					let name = this.starter.querySelector(".employee-card__name");
-					let position = this.starter.querySelector(".employee-card__position");
-					let description = this.starter.querySelector(".employee-card__description");
-					employeeName.innerHTML = name.innerHTML;
-					employeePosition.innerHTML = position.innerHTML;
-					employeeDescription.innerHTML = description.innerHTML;
-				}
+// 				if (employeeName && employeePosition && employeeDescription) {
+// 					let name = this.starter.querySelector(".employee-card__name");
+// 					let position = this.starter.querySelector(".employee-card__position");
+// 					let description = this.starter.querySelector(".employee-card__description");
+// 					employeeName.innerHTML = name.innerHTML;
+// 					employeePosition.innerHTML = position.innerHTML;
+// 					employeeDescription.innerHTML = description.innerHTML;
+// 				}
 
-				//life
-				let lifeTitle = this._nextWindows.querySelector('.modal__life-title');
-				let lifeText = this._nextWindows.querySelector('.modal__life-text');
+// 				//life
+// 				let lifeTitle = this._nextWindows.querySelector('.modal__life-title');
+// 				let lifeText = this._nextWindows.querySelector('.modal__life-text');
 
-				if (lifeText && lifeTitle) {
-					let title = this.starter.parentElement.querySelector(".life__block-title");
-					let text = this.starter.parentElement.querySelector(".life__text-block");
-					lifeTitle.innerHTML = title.innerHTML;
-					lifeText.innerHTML = text.innerHTML;
-				}
+// 				if (lifeText && lifeTitle) {
+// 					let title = this.starter.parentElement.querySelector(".life__block-title");
+// 					let text = this.starter.parentElement.querySelector(".life__text-block");
+// 					lifeTitle.innerHTML = title.innerHTML;
+// 					lifeText.innerHTML = text.innerHTML;
+// 				}
 
-				this.open();
-				return;
-			}
-			if (e.target.closest(`[${this.config.closeBtnAttrName}]`)) {
-				this.close();
-			}
-		});
+// 				this.open();
+// 				return;
+// 			}
+// 			if (e.target.closest(`[${this.config.closeBtnAttrName}]`)) {
+// 				this.close();
+// 			}
+// 		});
 
-		if (this.config.closeOnOverlay) {
-			document.addEventListener('mousedown', (e) => {
-				if (!this._isMoved && (e.target instanceof Element) && !e.target.classList.contains('modal__wrapper')) return;
-				this._overlayChecker = true;
-			});
+// 		if (this.config.closeOnOverlay) {
+// 			document.addEventListener('mousedown', (e) => {
+// 				if (!this._isMoved && (e.target instanceof Element) && !e.target.classList.contains('modal__wrapper')) return;
+// 				this._overlayChecker = true;
+// 			});
 
-			document.addEventListener('mouseup', (e) => {
-				if (!this._isMoved && (e.target instanceof Element) && this._overlayChecker && e.target.classList.contains('modal__wrapper')) {
-					e.preventDefault();
-					this._overlayChecker = !this._overlayChecker;
-					this.close();
-					return;
-				}
-				this._overlayChecker = false;
-			});
-		}
+// 			document.addEventListener('mouseup', (e) => {
+// 				if (!this._isMoved && (e.target instanceof Element) && this._overlayChecker && e.target.classList.contains('modal__wrapper')) {
+// 					e.preventDefault();
+// 					this._overlayChecker = !this._overlayChecker;
+// 					this.close();
+// 					return;
+// 				}
+// 				this._overlayChecker = false;
+// 			});
+// 		}
 
-		window.addEventListener('keydown', (e) => {
-			if (!this._isMoved && this.config.closeOnEsc && e.which === 27 && this.isOpened) {
-				e.preventDefault();
-				this.close();
-				return;
-			}
-			if (!this._isMoved && this.config.catchFocus && e.which === 9 && this.isOpened) {
-				this.focusCatcher(e);
-			}
-		});
-	}
+// 		window.addEventListener('keydown', (e) => {
+// 			if (!this._isMoved && this.config.closeOnEsc && e.which === 27 && this.isOpened) {
+// 				e.preventDefault();
+// 				this.close();
+// 				return;
+// 			}
+// 			if (!this._isMoved && this.config.catchFocus && e.which === 9 && this.isOpened) {
+// 				this.focusCatcher(e);
+// 			}
+// 		});
+// 	}
 
-	open(selector) {
-		if (selector) {
-			if (typeof (selector) === 'string') {
-				this._nextWindows = document.querySelector(selector);
-			} else {
-				this._nextWindows = selector;
-			}
-		}
-		if (!this._nextWindows) {
-			console.log('Warning: Modal selector is not found');
-			return;
-		}
-		if (this.isOpened) {
-			this._reopenTrigger = true;
-			this.close();
-			return;
-		}
-		this.openedWindow = this._nextWindows;
+// 	open(selector) {
+// 		if (selector) {
+// 			if (typeof (selector) === 'string') {
+// 				this._nextWindows = document.querySelector(selector);
+// 			} else {
+// 				this._nextWindows = selector;
+// 			}
+// 		}
+// 		if (!this._nextWindows) {
+// 			console.log('Warning: Modal selector is not found');
+// 			return;
+// 		}
+// 		if (this.isOpened) {
+// 			this._reopenTrigger = true;
+// 			this.close();
+// 			return;
+// 		}
+// 		this.openedWindow = this._nextWindows;
 
-		this._form = this.openedWindow.querySelector('form');
+// 		this._form = this.openedWindow.querySelector('form');
 
-		this.config.beforeOpen(this);
-		this._bodyScrollControl();
+// 		this.config.beforeOpen(this);
+// 		this._bodyScrollControl();
 
-		this.openedWindow.classList.add('modal--active');
-		this.openedWindow.setAttribute('aria-hidden', 'false');
-		if (this.config.catchFocus) this.focusControl();
-		this.isOpened = true;
-	}
+// 		this.openedWindow.classList.add('modal--active');
+// 		this.openedWindow.setAttribute('aria-hidden', 'false');
+// 		if (this.config.catchFocus) this.focusControl();
+// 		this.isOpened = true;
+// 	}
 
-	close() {
-		if (!this.isOpened) {
-			return;
-		}
+// 	close() {
+// 		if (!this.isOpened) {
+// 			return;
+// 		}
 
-		if (this._form) {
-			let hiddenInputs = this._form.querySelectorAll('input[data-form-data]');
-			hiddenInputs.forEach(input => {
-				this._form.removeChild(input);
-			});
-		}
+// 		if (this._form) {
+// 			let hiddenInputs = this._form.querySelectorAll('input[data-form-data]');
+// 			hiddenInputs.forEach(input => {
+// 				this._form.removeChild(input);
+// 			});
+// 		}
 
-		this.openedWindow.classList.add('modal--moved');
-		this._isMoved = true;
-		this.openedWindow.addEventListener('transitionend', this._closeAfterTransition);
-		this.openedWindow.classList.remove('modal--active');
-	}
+// 		this.openedWindow.classList.add('modal--moved');
+// 		this._isMoved = true;
+// 		this.openedWindow.addEventListener('transitionend', this._closeAfterTransition);
+// 		this.openedWindow.classList.remove('modal--active');
+// 	}
 
-	_closeAfterTransition() {
-		this.openedWindow.classList.remove('modal--moved');
-		this.openedWindow.removeEventListener('transitionend', this._closeAfterTransition);
-		this._isMoved = false;
-		this.openedWindow.setAttribute('aria-hidden', 'true');
+// 	_closeAfterTransition() {
+// 		this.openedWindow.classList.remove('modal--moved');
+// 		this.openedWindow.removeEventListener('transitionend', this._closeAfterTransition);
+// 		this._isMoved = false;
+// 		this.openedWindow.setAttribute('aria-hidden', 'true');
 
-		if (this.config.catchFocus) this.focusControl();
-		this._bodyScrollControl();
-		this.isOpened = false;
-		this.openedWindow.scrollTop = 0;
-		this.config.afterClose(this);
+// 		if (this.config.catchFocus) this.focusControl();
+// 		this._bodyScrollControl();
+// 		this.isOpened = false;
+// 		this.openedWindow.scrollTop = 0;
+// 		this.config.afterClose(this);
 
-		if (this._reopenTrigger) {
-			this._reopenTrigger = false;
-			this.open();
-		}
-	}
+// 		if (this._reopenTrigger) {
+// 			this._reopenTrigger = false;
+// 			this.open();
+// 		}
+// 	}
 
-	focusControl() {
-		const nodes = this.openedWindow.querySelectorAll(this._focusElements);
-		if (this.isOpened && this.starter) {
-			this.starter.focus();
-		} else if (nodes.length) nodes[0].focus();
-	}
+// 	focusControl() {
+// 		const nodes = this.openedWindow.querySelectorAll(this._focusElements);
+// 		if (this.isOpened && this.starter) {
+// 			this.starter.focus();
+// 		} else if (nodes.length) nodes[0].focus();
+// 	}
 
-	focusCatcher(e) {
-		const nodes = this.openedWindow.querySelectorAll(this._focusElements);
-		const nodesArray = Array.prototype.slice.call(nodes);
-		if (!this.openedWindow.contains(document.activeElement)) {
-			nodesArray[0].focus();
-			e.preventDefault();
-		} else {
-			const focusedItemIndex = nodesArray.indexOf(document.activeElement);
-			if (e.shiftKey && focusedItemIndex === 0) {
-				nodesArray[nodesArray.length - 1].focus();
-				e.preventDefault();
-			}
-			if (!e.shiftKey && focusedItemIndex === nodesArray.length - 1) {
-				nodesArray[0].focus();
-				e.preventDefault();
-			}
-		}
-	}
+// 	focusCatcher(e) {
+// 		const nodes = this.openedWindow.querySelectorAll(this._focusElements);
+// 		const nodesArray = Array.prototype.slice.call(nodes);
+// 		if (!this.openedWindow.contains(document.activeElement)) {
+// 			nodesArray[0].focus();
+// 			e.preventDefault();
+// 		} else {
+// 			const focusedItemIndex = nodesArray.indexOf(document.activeElement);
+// 			if (e.shiftKey && focusedItemIndex === 0) {
+// 				nodesArray[nodesArray.length - 1].focus();
+// 				e.preventDefault();
+// 			}
+// 			if (!e.shiftKey && focusedItemIndex === nodesArray.length - 1) {
+// 				nodesArray[0].focus();
+// 				e.preventDefault();
+// 			}
+// 		}
+// 	}
 
-	_bodyScrollControl() {
-		if (!this.config.backscroll) return;
+// 	_bodyScrollControl() {
+// 		if (!this.config.backscroll) return;
 
-		const fixedSelectorsElems = document.querySelectorAll(this.config.fixedSelectors);
-		const fixedSelectors = Array.prototype.slice.call(fixedSelectorsElems);
+// 		const fixedSelectorsElems = document.querySelectorAll(this.config.fixedSelectors);
+// 		const fixedSelectors = Array.prototype.slice.call(fixedSelectorsElems);
 
-		const html = document.querySelector('.main');
-		if (this.isOpened === true) {
-			html.classList.remove('modal-opened');
-			html.style.paddingRight = '';
-			fixedSelectors.forEach((el) => {
-				el.style.paddingRight = '';
-			});
-			window.scrollTo(0, this._scrollPosition);
-			html.style.top = '';
-			return;
-		}
-		this._scrollPosition = window.pageYOffset;
-		const marginSize = window.innerWidth - html.clientWidth;
-		html.style.top = `${-this._scrollPosition}px`;
+// 		const html = document.querySelector('.main');
+// 		if (this.isOpened === true) {
+// 			html.classList.remove('modal-opened');
+// 			html.style.paddingRight = '';
+// 			fixedSelectors.forEach((el) => {
+// 				el.style.paddingRight = '';
+// 			});
+// 			window.scrollTo(0, this._scrollPosition);
+// 			html.style.top = '';
+// 			return;
+// 		}
+// 		this._scrollPosition = window.pageYOffset;
+// 		const marginSize = window.innerWidth - html.clientWidth;
+// 		html.style.top = `${-this._scrollPosition}px`;
 
-		if (marginSize) {
-			html.style.paddingRight = `${marginSize}px`;
-			fixedSelectors.forEach((el) => {
-				el.style.paddingRight = `${parseInt(getComputedStyle(el).paddingRight, 10) + marginSize}px`;
-			});
-		}
-		html.classList.add('modal-opened');
-	}
-}
+// 		if (marginSize) {
+// 			html.style.paddingRight = `${marginSize}px`;
+// 			fixedSelectors.forEach((el) => {
+// 				el.style.paddingRight = `${parseInt(getComputedStyle(el).paddingRight, 10) + marginSize}px`;
+// 			});
+// 		}
+// 		html.classList.add('modal-opened');
+// 	}
+// }
 
 class Accordion {
 	constructor(target, config) {
@@ -418,7 +418,7 @@ $(document).ready(function () {
 	 * Modal
 	 */
 
-	let modals = new Modal();
+	// let modals = new Modal();
 
 
 	/**
